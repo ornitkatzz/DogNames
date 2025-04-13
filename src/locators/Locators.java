2ea059e4e16f60cdc8aebed215773be5b9d963a2
ornitkatzz <2ornit@gmail.com>
2025-04-13 20:51:54 +0300

    Update Locators.java
    better locators

--- src/locators/Locators.java
+++ src/locators/Locators.java
@@ -3,16 +3,16 @@ package locators;
 import org.openqa.selenium.By;
 
 public class Locators {
-	public static final By MAIN_TITLE = By.tagName("h1");
+    public static final By MAIN_TITLE = By.tagName("h1");
     public static final By BODY = By.tagName("body");
-    public static final By MENU_BUTTON = By.xpath("//div/nav/ul/li/a");
+    public static final By MENU_BUTTON = By.cssSelector("nav ul li a");
     public static final By X_BUTTON = By.className("close");
     public static final By MENU = By.id("menu");
-    public static final By MENU_ITEMS = By.xpath("//*[@id=\"menu\"]/div/ul/li");
-    public static final By MAIN_MENU_ITEMS = By.xpath("//*[@id=\"main\"]/div/section/article");
+    public static final By MENU_ITEMS = By.cssSelector("#menu > div > ul > li");
+    public static final By MAIN_MENU_ITEMS = By.cssSelector("#main > div > section > article");
     public static final By NAME_INPUT = By.id("name");
     public static final By EMAIL_INPUT = By.id("email");
     public static final By MESSAGE_INPUT = By.id("message");
     public static final By SEND_BUTTON = By.className("primary");
-    public static final By ERROR_MSG = By.xpath("//*[text()='This XML file does not appear to have any style information associated with it. The document tree is shown below.']");
+    public static final By ERROR_MSG = By.xpath("//*[contains(text(),'This XML file does not appear to have any style')]");
 }
